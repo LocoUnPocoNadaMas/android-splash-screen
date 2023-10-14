8dea9e46754ba48a9287ca785714f4956affa4df
Pato <dkno_c1@hotmail.com>
2023-10-14 05:12:25 -0300

    splash screen working... could be better

---
+++ app/src/main/java/com/pato/splashscreen_migrated/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.pato.splashscreen_migrated
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.pato.splashscreen_migrated.ui.theme.SplashScreenMigratedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //val splashScreen = installSplashScreen();
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreenMigratedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SplashScreenMigratedTheme {
+        Greeting("Android")
+    }
+}
